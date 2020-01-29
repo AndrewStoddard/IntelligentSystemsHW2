0daafdb38a2aeb6264a631d628718b3f638fb1c5
Andrew Stoddard <acs8182000@gmail.com>
2020-01-29 15:48:34 -0500

    Added the DFS Tests

--- src/edu/westga/cs3270/search/test/dfs/TestDFSearch.java
+++ src/edu/westga/cs3270/search/test/dfs/TestDFSearch.java
@@ -43,4 +43,67 @@ public class TestDFSearch {
 		assertEquals("[Start, A, C, Goal]", dfs.search());
 	}
 
+
+	
+	@Test
+	void testIfSearchGoesLeftFirst() {
+		State successorCR = new State("T2A", null, null);
+		State successorAL = new State("T2Goal", null, null);
+		State start = new State("T2Start", successorCR, successorAL);
+
+		DepthFirstSearch dfs = new DepthFirstSearch(start, successorAL);
+		assertEquals("[T2Start, T2A, T2Goal]", dfs.search());
+	}
+
+	@Test
+	void testFirstStateIsGoalStateWithNothingAfter() {
+		State goalState = new State("T3Goal", null, null);
+
+		DepthFirstSearch dfs = new DepthFirstSearch(goalState, goalState);
+		assertEquals("[T3Goal]", dfs.search());
+	}
+
+	@Test
+	void testFirstStateIsGoalStateWithFollowingStates() {
+		State successorBL = new State("T4E", null, null);
+		State successorBR = new State("T4F", null, null);
+		State successorCL = new State("T4Goal", successorBL, successorBR);
+
+		DepthFirstSearch dfs = new DepthFirstSearch(successorCL, successorCL);
+		assertEquals("[T4Goal]", dfs.search());
+	}
+
+	@Test
+	void testGoalStateHasStatesBeforeAndAfter() {
+		State successorCR = new State("T5H", null, null);
+		State successorAL = new State("T5Goal", null, successorCR);
+		State successorAR = new State("T5D", null, null);
+		State successorSL = new State("T5A", successorAL, successorAR);
+		State successorBL = new State("T5E", null, null);
+		State successorBR = new State("T5F", null, null);
+		State successorSR = new State("T5B", successorBL, successorBR);
+		State start = new State("T5Start", successorSL, successorSR);
+
+		DepthFirstSearch dfs = new DepthFirstSearch(start, successorAL);
+		assertEquals("[T5Start, T5A, T5Goal]", dfs.search());
+	}
+
+	@Test
+	void testUnreachableGoalState() {
+		State successorCL = new State("T6Goal", null, null);
+		State successorCR = new State("T6H", null, null);
+		State successorAL = new State("T6C", null, successorCR);
+		State successorAR = new State("T6D", null, null);
+		State successorSL = new State("T6A", successorAL, successorAR);
+		State successorBL = new State("T6E", null, null);
+		State successorBR = new State("T6F", null, null);
+		State successorSR = new State("T6B", successorBL, successorBR);
+		State start = new State("T6Start", successorSL, successorSR);
+
+		DepthFirstSearch dfs = new DepthFirstSearch(start, successorCL);
+		assertEquals("[T6Start, T6A, T6C, T6H, T6D, T6B, T6E, T6F]", dfs.search());
+	}
+
+
+
 }
